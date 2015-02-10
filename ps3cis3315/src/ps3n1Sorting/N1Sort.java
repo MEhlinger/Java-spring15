fce78340eebd4df263f72b30b2f839a12d9d8eeb
CSLab <CSLab@MUP-C-EHCSL08.MANSFIELD.LCL>
2015-02-10 09:22:51 -0500

    Completed N1Sort Selection Sort class, with pseudorandom test array

--- ps3cis3315/src/ps3n1Sorting/N1Sort.java
+++ ps3cis3315/src/ps3n1Sorting/N1Sort.java
@@ -1,5 +1,7 @@
 package ps3n1Sorting;
 
+import java.util.Random;
+
 /**
  * Implement a selection sort similar to listing 7.8 and use it to sort an array
  * of numbers. Estimate the execution time to sort 1000, 10000, 100000, and
@@ -11,6 +13,37 @@ package ps3n1Sorting;
 public class N1Sort {
 
     public static void main(String[] args) {
-
+        Random rand = new Random();
+        int[] a = new int[10];
+        for (int i =0; i < a.length; i++) {
+            a[i] = rand.nextInt(a.length);
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+        selectionSort(a);
+        for (int val : a) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+        
+    }
+    
+    public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int currentMin = array[i];
+            int currentMinIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (currentMin > array[j]) {
+                    currentMin = array[j];
+                    currentMinIndex = j;
+                }
+            }
+            // After the 'j' loop pass through all array elements, move new
+            // smallest number to front, then increment the i counter and repeat
+            if (currentMinIndex != i) {
+                array[currentMinIndex] = array[i];
+                array[i] = currentMin;
+            }
+        }
     }
 }
