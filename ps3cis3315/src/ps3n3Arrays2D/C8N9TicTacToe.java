8abfda82d30957077b1ad7b4c593e81bbe66c4e0
Marshall Ehlinger <mehlinger333@gmail.com>
2015-02-13 11:21:16 -0500

    Initial WIP commit of tictactoe- builds and draws empty board

--- ps3cis3315/src/ps3n3Arrays2D/C8N9TicTacToe.java
+++ ps3cis3315/src/ps3n3Arrays2D/C8N9TicTacToe.java
@@ -9,7 +9,44 @@ package ps3n3Arrays2D;
 public class C8N9TicTacToe {
 
     public static void main(String[] args) {
-        int[][] board = new int[3][3];
-
+        String[][] board = new String[3][3];
+        populateEmptyBoard(board);
+        drawBoard(board);
+        while (true) {
+            //Game logic
+        }
+    }
+    
+    public static void populateEmptyBoard(String[][] gameBoard) {
+        // Populates empty gameboard with blank space strings for console display
+        for (int c = 0; c < gameBoard.length; c++) {
+            for (int r = 0; r < gameBoard[c].length; r ++) {
+                gameBoard[c][r] = " ";
+            
+            }
+        }
+    }
+    
+    public static void drawBoard(String[][] gameBoard) {
+        // Clears console and prints new board
+        for (int c = 0; c < gameBoard.length; c++) {
+            for (int r = 0; r < gameBoard[c].length; r ++) {
+                System.out.print("|" + gameBoard[c][r]);
+                }
+            System.out.println("|");
+            }
+        }
+    
+    public static void playerMove(String[][] gameBoard) {
+        // Allows the player to make a single move, using the numpad layout
+    }
+    
+    public static void botMove(String[][] gameBoard) {
+        // Allows the NPC bot to make a single move
+    }
+    
+    public static void checkWinner(String[][] gameBoard) {
+        // Processes gameboard to determine if there is a winner
     }
 }
+ 
