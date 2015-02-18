3073e0fbceebf10550398b8dd534fd6520f3153a
Marshall Ehlinger <ehlingermd15@mounties.mansfield.edu>
2015-02-18 15:10:31 -0500

    Completed fan practice class assignment, as well as test class

--- ps3cis3315/src/ps3n5Chapter9/P9N8Fan.java
+++ ps3cis3315/src/ps3n5Chapter9/P9N8Fan.java
@@ -1,15 +1,81 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package ps3n5Chapter9;
 
 /**
  *
- * @author CSLab
+ * @author Narshall Ehlinger
  */
+
 public class P9N8Fan {
+    // Speed constants
+    final int SLOW = 1;
+    final int MEDIUM = 2;
+    final int FAST = 3;
     
+    // Nonconstant variable initializations
+    private int speed = SLOW;
+    private boolean on = false;
+    private double radius = 5;
+    private String color = "blue";
+
+    
+    // Accessors and Mutators
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(String speed) {
+        switch (speed) {
+            case "SLOW":
+                this.speed = SLOW;
+                break;
+            case "MEDIUM":
+                this.speed = MEDIUM;
+                break;
+            case "FAST":
+                this.speed = FAST;
+                break;
+            default:
+                System.out.println("Speed set failed... pass SLOW, MEDIUM, or FAST.");
+                break;
+        }
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    // Constructors
+    
+    public P9N8Fan() {
+        // No arg constructor
+    }
+
+    @Override
+    public String toString() {
+        if (isOn()) {
+            return "Speed= " + this.speed + ", Color= " + this.color + ", Radius= " + this.radius;
+        } else {
+            return "Color= " + this.color + ", Radius= " + this.radius + ", FAN IS OFF.";
+        }
+    }
 }
--- ps3cis3315/src/ps3n5Chapter9/P9N8Test.java
+++ ps3cis3315/src/ps3n5Chapter9/P9N8Test.java
@@ -1,15 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package ps3n5Chapter9;
 
 /**
  *
- * @author CSLab
+ * @author Marshall Ehlinger
  */
+
 public class P9N8Test {
-    
+    public static void main (String[] args) {
+        P9N8Fan f1 = new P9N8Fan();
+        P9N8Fan f2 = new P9N8Fan();
+
+        f1.setSpeed("FAST");
+        f1.setColor("yellow");
+        f1.setRadius(10);
+        f1.setOn(true);
+     
+        f2.setSpeed("MEDIUM");
+        f2.setColor("blue");
+        f2.setRadius(5);
+        f2.setOn(false);
+        
+        System.out.println("f1 : " + f1.toString());
+        System.out.println("f2 : " + f2.toString());
+        
+    }
 }
