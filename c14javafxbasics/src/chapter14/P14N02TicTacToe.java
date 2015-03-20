fad129ebf4af21f1324d5fb9dc43815da4f10010
Marshall Ehlinger <mehlinger333@gmail.com>
2015-03-20 09:30:13 -0400

    Added basic javaFX gui tictactoe board with buttons and alternating xs and os

--- c14javafxbasics/src/chapter14/P14N02TicTacToe.java
+++ c14javafxbasics/src/chapter14/P14N02TicTacToe.java
@@ -6,6 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -14,23 +15,59 @@ import javafx.stage.Stage;
  * @author Marshall Ehlinger
  */
 public class P14N02TicTacToe extends Application {
-    
+    Button[] btn = new Button[9];
+    int count = 1;
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+        GridPane gpane = new GridPane();
+
+        // Create buttons
+        for (int i = 0; i < btn.length; i++) {
+            btn[i] = new Button();
+            btn[i].setText("");
+            btn[i].setMinWidth(40);
+            btn[i].setMinHeight(40);
+            btn[i].setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    Button b = (Button) event.getSource();
+                    b.setText(count++ % 2 == 0 ? "X" : "O");
+                }
+            });
+        }
+
+        // Place buttons on gridpane
+        for (int row = 0; row < 3; row++){
+            for (int col = 0; col < 3; col++) {
+                gpane.add(btn[row * 3 + col], col, row);
             }
-        });
+        }
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+//        Button btn00 = new Button();
+//        btn00.setText("X");
+//        btn00.setOnAction(new EventHandler<ActionEvent>() {
+//            
+//            @Override
+//            public void handle(ActionEvent event) {
+//                System.out.println("Hello World!");
+//            }
+//        });
+//        
+//        Button btn01 = new Button();
+//        btn01.setText("O");
+//        btn01.setOnAction(new EventHandler<ActionEvent>() {
+//            
+//            @Override
+//            public void handle(ActionEvent event) {
+//                System.out.println("Hello World!");
+//            }
+//        });
+//        
+//        
+//        gpane.getChildren().add(btn00);
+//        gpane.getChildren().add(btn01);
         
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(gpane, 300, 250);
         
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
