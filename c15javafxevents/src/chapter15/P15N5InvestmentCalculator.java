af3d7c4427dfcc6bd1732ce2492b7a160b23cc85
Marshall Ehlinger <ehlingermd15@mounties.mansfield.edu>
2015-03-26 17:02:09 -0400

    Investment Calculator completed

--- c15javafxevents/src/chapter15/P15N5InvestmentCalculator.java
+++ c15javafxevents/src/chapter15/P15N5InvestmentCalculator.java
@@ -7,7 +7,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 /**
@@ -19,33 +22,40 @@ import javafx.stage.Stage;
  */
 public class P15N5InvestmentCalculator extends Application {
  
-    private TextField tfInitInvest = new TextField();
-    private TextField tfInterestRate = new TextField();
-    private TextField tfNumYears = new TextField();
+    private TextField tfInitInvest = new TextField("100");
+    private TextField tfInterestRate = new TextField(".05");
+    private TextField tfNumYears = new TextField("15");
     private Button calcB = new Button();
 
     
     @Override
     public void start(Stage primaryStage) {
         
+        BorderPane wrapper = new BorderPane();
         GridPane pane = new GridPane();
+        
+        wrapper.setCenter(pane);
         
         
         pane.setHgap(20);
         pane.setVgap(5);
         
+        Label title = new Label("INVESTMENT CALCULATOR");
+        title.setFont(Font.font("Monospace", FontWeight.BOLD, 25));
+        wrapper.setTop(title);
+        
         pane.add(new Label("Initial Investment: "), 0, 0);
         pane.add(tfInitInvest, 1, 0);
-        pane.add(new Label("Initial Investment: "), 0, 1);
+        pane.add(new Label("Interest Rate: "), 0, 1);
         pane.add(tfInterestRate, 1, 1);
-        pane.add(new Label("Initial Investment: "), 0, 2);
+        pane.add(new Label("Number of Years: "), 0, 2);
         pane.add(tfNumYears, 1, 2);
         
         calcB.setText("Calculate Value");
-        pane.getChildren().add(calcB);
+        pane.add(calcB, 0, 3);
         
-        Label lResult = new Label("Enter Values and Press Calculate...");
-        pane.getChildren().add(lResult);
+        Label lResult = new Label("Enter Values...");
+        pane.add(lResult, 0, 4);
         
         calcB.setOnAction(e -> { 
             final double result = calculateInvestment();
@@ -53,6 +63,7 @@ public class P15N5InvestmentCalculator extends Application {
                 });
         
         // UI Alignments
+        BorderPane.setAlignment(title, Pos.CENTER);
         pane.setAlignment(Pos.CENTER);
         tfInitInvest.setAlignment(Pos.BOTTOM_RIGHT);
         tfInterestRate.setAlignment(Pos.BOTTOM_RIGHT);
@@ -60,17 +71,20 @@ public class P15N5InvestmentCalculator extends Application {
         calcB.setAlignment(Pos.BASELINE_CENTER);
         
         
-        Scene scene = new Scene(pane, 500, 400);
+        Scene scene = new Scene(wrapper, 500, 400);
         
-        primaryStage.setTitle("Loan Calculator");
+        primaryStage.setTitle("Investment Calculator");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
     
     private double calculateInvestment() {
-        double init = 10;//Double.parseDouble(tfInitInvest.getText());
-        double rate = .01;//Double.parseDouble(tfInterestRate.getText());
-        double years = 20;//Double.parseDouble(tfNumYears.getText());
+        // NOTE: no input validation.
+        // KNOWN BUG: crashes if provided with empty textfield
+        double init = Double.parseDouble(tfInitInvest.getText());
+        double rate = Double.parseDouble(tfInterestRate.getText());
+        double years = Double.parseDouble(tfNumYears.getText());
+       
         return init + ((init * rate) * years);
     }
 
