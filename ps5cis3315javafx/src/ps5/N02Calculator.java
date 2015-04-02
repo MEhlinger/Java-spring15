8303fd4efb9443be563c0015779a2cda9d3225a9
Marshall Ehlinger <mehlinger333@gmail.com>
2015-04-02 09:39:09 -0400

    Added N02Calculator buttons and output field, no functionality

--- ps5cis3315javafx/src/ps5/N02Calculator.java
+++ ps5cis3315javafx/src/ps5/N02Calculator.java
@@ -3,38 +3,70 @@ package ps5;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 /**
  * N02Calculator Write a program that implements a simple business calculator.
  *
- * @author Your name here
+ * @author Marshall Ehlinger
  */
 public class N02Calculator extends Application {
 
+    GridPane gp = new GridPane();
+    Button[][] btn = new Button[4][4];
+    TextField tfResult;
+    
+    
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(root, 300, 250);
-
-        primaryStage.setTitle("Hello World!");
+        BorderPane bpane = new BorderPane();
+        
+        bpane.setCenter(gp);
+        
+        tfResult = new TextField(". . .");
+        tfResult.setEditable(false);
+        tfResult.setAlignment(Pos.BOTTOM_RIGHT);
+        tfResult.setFont(Font.font("Monospace", 36));
+        tfResult.setMinWidth(400);
+        bpane.setTop(tfResult);
+        
+        bpane.setCenter(getButtonGridPane());
+        
+        Scene scene = new Scene(bpane);
+        
+        primaryStage.setTitle("MarshallCalc");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    
+    protected GridPane getButtonGridPane() {
+        gp = new GridPane();
+        String [][] buttonNameArray = {
+            {"7", "8", "9", "/"},
+            {"4", "5", "6", "*"},
+            {"1", "2", "3", "-"},
+            {"0", ".", "=", "+"}
+        };
+        
+        ButtonHandler bh = new ButtonHandler();
+        for (int row = 0; row < buttonNameArray.length; row++) {
+            for (int col = 0; col < buttonNameArray[0].length; col++) {
+                btn[row][col] = new Button(buttonNameArray[row][col]);
+                btn[row][col].setStyle("-fx-base: #ad319f; -fx-font-size: 36 monospace");
+                btn[row][col].setMinSize(80,80);
+                
+                btn[row][col].setOnAction(bh);
+                gp.add(btn[row][col], col, row);
+            }
+        }
+        return gp;
     }
 
     /**
@@ -42,6 +74,14 @@ public class N02Calculator extends Application {
      */
     public static void main(String[] args) {
         launch(args);
+    }
+
+    class ButtonHandler implements EventHandler<ActionEvent> {
+          
+        @Override
+        public void handle(ActionEvent event) {
+            System.out.println("Button Clicked.");
+        }
     }
 
 }
