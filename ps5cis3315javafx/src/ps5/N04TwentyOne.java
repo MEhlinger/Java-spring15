acddfd3d200c20cec7ef51a24b35a208f89df0f4
Marshall Ehlinger <ehlingermd15@mounties.mansfield.edu>
2015-04-09 17:52:43 -0400

    N04TwentyOne: Added styling to buttons and layed out panes

--- ps5cis3315javafx/src/ps5/N04TwentyOne.java
+++ ps5cis3315javafx/src/ps5/N04TwentyOne.java
@@ -1,10 +1,13 @@
 package ps5;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
@@ -28,6 +31,8 @@ public class N04TwentyOne extends Application {
     @Override
     public void start(Stage primaryStage) {
             
+        /* IMAGES AND LABELS*/
+        
        for (int i = 0; i < cardFiles.length; i++) {
            cardFiles[i] = new Image("mycards/c" + i + ".gif");
        }
@@ -37,8 +42,13 @@ public class N04TwentyOne extends Application {
        lblPlayer.setText("PLAYER: 00");
        lblPlayer.setStyle("-fx-font-size: 22pt; -fx-font-weight: bolder; -fx-text-fill: #445566; -fx-padding: 20");
        
-       pnDealer = new HBox(-80);
-       pnPlayer = new HBox(-80);
+       /* GRIDPANE */
+       
+       pnDealer = new HBox(-110);
+       pnDealer.setPadding(new Insets(0,10,10,10));
+       
+       pnPlayer = new HBox(-110);
+       pnPlayer.setPadding(new Insets(0,10,10,15));
        
        GridPane pnGrid = new GridPane();
        ColumnConstraints col1 = new ColumnConstraints();
@@ -52,30 +62,42 @@ public class N04TwentyOne extends Application {
        pnGrid.add(lblPlayer, 1, 0);
        pnGrid.add(pnPlayer, 1, 1); 
        
-       HBox pnButtons = new HBox();
+       /* UI BUTTONS */
+       
+       HBox pnButtons = new HBox(20);
+       String btnColor = "#ff4444";
        
        btnHit = new Button("Hit");
-       
+       btnHit.setMinWidth(150);
+       btnHit.setStyle("-fx-font-size: 14pt; -fx-text-fill: " + btnColor);
        btnStand = new Button("Stand");
+       btnStand.setMinWidth(150);
+       btnStand.setStyle("-fx-font-size: 14pt; -fx-text-fill: " + btnColor);
        
        btnNew = new Button("New");
+       btnNew.setMinWidth(150);
+       btnNew.setStyle("-fx-font-size: 14pt; -fx-text-fill: " + btnColor);
        btnNew.setOnAction(e -> newGame());
        
        pnButtons.getChildren().addAll(btnHit, btnStand, btnNew);
+       pnButtons.setAlignment(Pos.CENTER);
+       
+       /* ROOT PANE, SCENE, AND STAGE */
        
        BorderPane pnBorder = new BorderPane();
        pnBorder.setCenter(pnGrid);
        pnBorder.setBottom(pnButtons);
               
         
-        Scene scene = new Scene(pnBorder, 500, 250);
+        Scene scene = new Scene(pnBorder, 750, 400);
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
     
     public void newGame() {
-        //pnDealer.getChildren().addAll(new ImageView(cardFiles[52]), new ImageView(cardFiles[0]));
+        pnDealer.getChildren().addAll(new ImageView(cardFiles[52]), new ImageView(cardFiles[0]));
+        pnPlayer.getChildren().addAll(new ImageView(cardFiles[42]), new ImageView(cardFiles[15]));
     }
 
     /**
